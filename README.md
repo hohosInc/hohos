30578781d3f3cf55f9f5cb2f4af3591a93cb3ec4
Deepak Bharti <deepakbharti823@gmail.com>
2018-03-22 20:30:58 +0530

    removed all

--- README.md
+++
@@ -1,3 +0,0 @@
-<h1 style="text-center">hohos</h1>
-
-A social network where expressions speak and things talk 
--- fun/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def funhome(request):
-	return render(request, 'fun/gattu_barat.html', {}) 
-
-def fungone(request):
-	return render(request, 'fun/banned.html', {})
